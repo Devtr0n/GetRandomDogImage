20c5677e4d5a892bd51f2ba54ab07ffb52e524a8
Richard Hollon <richardsmailbox@gmail.com>
2024-12-31 10:05:54 -0600

    Add files via upload
    Get Random Dog Image script example

---
+++ Get-RandomDogImage.ps1
@@ -0,0 +1,17 @@
+﻿function Get-RandomDogImage {
+    $url = "https://dog.ceo/api/breeds/image/random"
+    try {
+
+        # Get a dog image
+        $response = Invoke-RestMethod -Uri $url -Method Get
+        $imageUrl = $response.message
+        Write-Host "Random Dog Image URL: $imageUrl"
+        
+        # Open the dog image in Google Chrome
+        Start-Process "chrome.exe" $imageUrl
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
+set-alias randomdog Get-RandomDogImage
+set-alias dog randomdog
